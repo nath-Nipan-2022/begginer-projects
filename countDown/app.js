b14068557e3455102a6f0e68ba669e83ee2d9888
Nipan Debnath <debnathnipan719@gmail.com>
2023-04-12 21:02:59 +0530

    code refactored

--- countDown/app.js
+++ countDown/app.js
@@ -1,50 +1,80 @@
-let today = new Date();
+// declear the variables
+let birthday = 0;
+let timer = 0;
 
-const months = [
-  "jan",
-  "feb",
-  "mar",
-  "apr",
-  "may",
-  "jun",
-  "jul",
-  "aug",
-  "sep",
-  "okt",
-  "nov",
-  "dec",
-];
+// get date of birth
+function getDOB() {
+  const box = document.createElement("div");
+  box.className = "input-field";
 
-const byDefault = `${today.getDate() + 1} ${months[today.getMonth()]}`;
+  box.innerHTML =
+    "<label for='date'>Pick Your Birtday Date</label><input type = 'date' name = 'date' /> ";
 
-let dob = prompt(
-  "what is your birth date? write like this:" + byDefault,
-  byDefault
-);
+  document.querySelector("main").appendChild(box);
+}
+getDOB();
 
-const birthday = new Date(`${dob} ${today.getFullYear()}`);
+// take input
+document.querySelector(".input-field input").onchange = handleDateInput;
 
-const countDown = () => {
+function handleDateInput(e) {
+  const dob = e.target.value;
+  console.log(dob);
+  birthday = new Date(`${dob} `);
+  //note: this little space `${dob} `<- here is needed for exact start of the day
+  startTimer();
+}
+
+// render the timer elements
+const desc = document.querySelector("#desc");
+
+desc.innerHTML = `<div class="timer-area">
+  <h2><span id="d">00</span>days</h2>
+  <h2><span id="h">00</span>hours</h2>
+  <h2><span id="m">00</span>minutes</h2>
+  <h2><span id="s">00</span>seconds</h2>
+  <div>`;
+
+const days = desc.querySelector("#d");
+const hrs = desc.querySelector("#h");
+const mins = desc.querySelector("#m");
+const secs = desc.querySelector("#s");
+
+function countDown() {
   // on each iteration new time is needed
-  let d = new Date();
-  let diff = birthday.getTime() - d.getTime();
-  let days = diff / (24 * 60 * 60 * 1000);
-  // returns days in decimals
-  let daysLeft = Math.floor(days);
-  let hoursLeft = Math.floor((days * 24) % 24);
-  let minutesLeft = Math.floor((days * 24 * 60) % 60);
-  let secondsLeft = Math.floor((diff / 1000) % 60);
+  const td = new Date();
 
-  document.querySelector(
-    "#desc"
-  ).innerHTML = ` <h2>Your birthday is on ${dob}</h2> 
-	<h2>Time remaining: </h2> <h1>${daysLeft}days ${hoursLeft}h ${minutesLeft}min ${secondsLeft}s <h1>`;
+  const seconds = (birthday - td) / 1000;
+
+  const day = Math.floor(seconds / 3600 / 24);
+  const hours = Math.floor(seconds / 3600) % 24;
+  const minutes = Math.floor((seconds / 60) % 60);
+
+  days.innerText = day;
+  hrs.innerText = hours;
+  mins.innerText = minutes;
+  secs.innerText = Math.floor(seconds % 60);
 
   if (diff === 0) {
-    document.querySelector("#desc").innerHTML =
-      "HAPPY BIRTHDAY! MANY MANY HAPPY RETURNS OF THE DAY";
+    desc.innerHTML = `<div class="timer-area">
+        <h2><span>H</span></h2>
+        <h2><span>A</span></h2>
+        <h2><span>P</span></h2>
+        <h2><span>P</span></h2>
+        <h2><span>Y</span></h2>
+        <h2><span>B</span></h2>
+        <h2><span>I</span></h2>
+        <h2><span>R</span></h2>
+        <h2><span>T</span></h2>
+        <h2><span>H</span></h2>
+        <h2><span>D</span></h2>
+        <h2><span>A</span></h2>
+        <h2><span>Y</span></h2>
+     <div>`;
     clearInterval(timer);
   }
-};
+}
 
-let timer = setInterval(countDown, 1000);
+function startTimer() {
+  timer = setInterval(countDown, 1000);
+}
